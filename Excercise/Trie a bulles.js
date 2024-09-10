48b7ad7ca6bf96a62a8104ddd590b45dd3a2b8be
Abderrafie Elmrabet <53119836+AbderrafieElmrabet@users.noreply.github.com>
2024-09-10 13:37:41 +0100

    Update Trie a bulles.js

--- Excercise/Trie a bulles.js
+++ Excercise/Trie a bulles.js
@@ -17,22 +17,3 @@ for (i = 0; i < numbers.length; i++) {
   }
 }
 console.log(numbers)
-
-//--------------------------------------------------------------------------
-
-//selection sort
-
-let len = numbers.length;
-for (let i = 0; i < len; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < len; j++) {
-        if (numbers[j] < numbers[minIndex]) {
-            minIndex = j;
-        }
-    }
-    if (minIndex !== i) {
-        [numbers[i], numbers[minIndex]] = [numbers[minIndex], numbers[i]];
-    }
-}
-
-console.log(numbers)
