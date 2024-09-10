0fe8ebee7bae3f98ba471da9a70ed9081d15c567
Abderrafie Elmrabet <53119836+AbderrafieElmrabet@users.noreply.github.com>
2024-09-10 11:06:40 +0100

    Update trie-par-séléction.js

--- Excercise/trie-par-séléction.js
+++ Excercise/trie-par-séléction.js
@@ -1,24 +1,3 @@
-//bubble sort
-
-let numbers = [4, 2, 5, 9];
-
-let temps;
-for (i = 0; i < numbers.length; i++) {
-  for (j = i + 1; j < numbers.length; j++) {
-    if (numbers[i] > numbers[j]) {
-
-      //save value of numbers[i]
-      temps = numbers[i]
-
-      //swap position of numbers when first number is smaller than the second number
-      numbers[i] = numbers[j]
-      numbers[j] = temps
-    }
-  }
-}
-console.log(numbers)
-
-//--------------------------------------------------------------------------
 
 //selection sort
 
