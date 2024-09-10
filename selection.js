3735409b40788341412dc2c9b083aec891cba40d
Abderrafie Elmrabet <53119836+AbderrafieElmrabet@users.noreply.github.com>
2024-09-10 09:34:13 +0100

    Update selection.js

--- selection.js
+++ selection.js
@@ -1,13 +1,17 @@
-let numbers = [4,2,5,9];
+let numbers = [4, 2, 5, 9];
 
-let temps ;
-for(i = 0;i<numbers.length;i++){
-    for(j=i+1;j<numbers.length;j++){
-        if(numbers[i]<numbers[j]){
-          temps =  numbers[i]
-          numbers[i]=numbers[j]
-          numbers[j]=temps  
-        }
-     }
+let temps;
+for (i = 0; i < numbers.length; i++) {
+  for (j = i + 1; j < numbers.length; j++) {
+    if (numbers[i] > numbers[j]) {
+
+      //save value of numbers[i]
+      temps = numbers[i]
+
+      //swap position of numbers when first number is smaller than the second number
+      numbers[i] = numbers[j]
+      numbers[j] = temps
+    }
+  }
 }
 console.log(numbers)
