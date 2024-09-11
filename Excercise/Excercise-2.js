4ecdb11d65112c53e86a4d9f697fef33aa70acc7
Abderrafie Elmrabet <53119836+AbderrafieElmrabet@users.noreply.github.com>
2024-09-11 09:24:28 +0100

    Update Excercise-2.js

--- Excercise/Excercise-2.js
+++ Excercise/Excercise-2.js
@@ -1,10 +1,3 @@
-let arr = [3, 7, 3, 5, 10, 7];
+//Table manipulation.
+//Remove duplicates from a table.
 
-let tn;
-
-for (j = 0; j < arr.length; j++)
-    for (i = 0; i < arr.length; i++) {
-        if (arr[j] == arr[i]) {
-            console.log(arr[j])
-        }
-    }
